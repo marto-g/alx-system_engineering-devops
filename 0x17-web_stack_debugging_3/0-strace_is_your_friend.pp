4965763f59f5816a1a91d99c41cda91dcc61c78d
marto-g <gathimartin@gmail.com>
2023-06-07 17:00:51 +0300

    task3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
+Latest# Fix 500 error when a GET HTTP method is requested to Apache web server
 
 exec {'fix_wp':
   provider => shell,
